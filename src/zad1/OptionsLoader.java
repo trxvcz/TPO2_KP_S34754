47aaa525ea5aad0186aad5044430ed73cd8b4f8e
pawel <pawelkoc681@gmail.com>
2026-04-01 23:12:08 +0200

    feat: OptionsLoader

--- src/zad1/OptionsLoader.java
+++ src/zad1/OptionsLoader.java
@@ -7,11 +7,36 @@
 package zad1;
 
 
-//import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class OptionsLoader {
   public GeoTimeOptions load(String fileName) throws Exception {
-    return new GeoTimeOptions("TODO", List.of());
+    Yaml yaml = new Yaml();
+    try(InputStream inputStream = Files.newInputStream(Paths.get(fileName))) {
+      Map<String, Object> data = yaml.load(inputStream);
+
+      if  (data == null) {
+        throw new Exception("Plik jest pusty" + fileName);
+      }
+
+      String serverZoneId = (String) data.get("serverZoneId");
+      if (serverZoneId == null || serverZoneId.isBlank()) {
+        throw new IllegalArgumentException("Brak wymaganego pola: serverZoneId");
+      }
+
+      List<String> logLines = (List<String>) data.get("logLines");
+      if (logLines == null) {
+        logLines = new ArrayList<>();
+      }
+
+      return new GeoTimeOptions(serverZoneId, logLines);
+    }
   }
 }
